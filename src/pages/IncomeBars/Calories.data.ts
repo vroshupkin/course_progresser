8b51c42f9793723f37c1a18b19f6354a13835c5b
Vadim <vroshupkin1@gmail.com>
2023-08-12 04:13:39 +0300

    Добавлен генератор данных калорий

---
+++ src/pages/IncomeBars/Calories.data.ts
@@ -0,0 +1,22 @@
+import { DateHelper } from '../../common/date.helper';
+import { range } from '../../common/generator';
+import { randomInt } from '../../common/number.helper';
+
+export type TCaloriesData = {
+  date: Date,
+  val: number
+}
+
+export const caloriesData: TCaloriesData[] = 
+  range(60).map(v => 
+  {
+    return(
+      {
+        date: DateHelper.changeDays(new Date(), -v),
+        val: randomInt(0, 3000)
+      }
+    );
+  }
+  );
+  
+

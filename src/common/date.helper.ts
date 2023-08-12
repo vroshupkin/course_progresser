0529e98acce31acf4831e658ffe7280b621bd2ed
Vadim <vroshupkin1@gmail.com>
2023-08-12 04:12:05 +0300

    Подкорректирован  date helper

--- src/common/date.helper.ts
+++ src/common/date.helper.ts
@@ -9,10 +9,19 @@ export enum MONTH {
   January, February, March, April, May, June, July, August, September, October, November, December
 }
 
+
 export enum WEEK {
   Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
 }
 
+export const MONTH_NAME = {
+  ru: {
+    /** Именительный падеж */
+    nominative: [ 'январь', 'февраль', 'март', 'апрель' , 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь' ],
+    /** Родительный падеж */
+    genetive: [ 'января', 'февраля', 'марта', 'апреля' , 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря' ]
+  }
+};
 
 export const isLeapYear = (year: number | Date) => 
 {
@@ -43,7 +52,6 @@ export const isLeapYear = (year: number | Date) =>
 
 export const month_helper = 
 {
-  // FIX посмотреть где используется и заменить на enum
   ru: [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ],
   en: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
   count_of_days: [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ],
@@ -56,12 +64,10 @@ export const month_helper =
    */
   getName(order: number, lang = 'ru')
   {
-    if(lang == 'en')
-    {
-      return this.en[order];    
-    }
+
+    return lang == 'en'?
+      this.en[order] : this.ru[order];
     
-    return this.ru[order];
   },
 
   /**
@@ -73,7 +79,7 @@ export const month_helper =
   {
     const order = date.getMonth();
     
-    if(order == 2 && isLeapYear(date))
+    if(order == MONTH.February && isLeapYear(date))
     {
       return 29;
     }
@@ -133,43 +139,45 @@ export const month_helper =
     
     return days;
   },
-
-  /**
-   * Возвращает порядок дня начиная с 1 января 1970 года
-   */
-  getDayOfYear(date: Date)
-  {
-    return Math.floor(Number(date) / IN_MS.DAY);
-  }
+  
   
 };
+/*
+ * Возвращает порядок дня начиная с 1 января 1970 года
+ */
+export const DateHelper = {
+  DateToDayOrder: (date: Date) => Math.floor(Number(date) / IN_MS.DAY),
+  // 12
+  DayOrderToDate: (dayOrder: number) => new Date(dayOrder * IN_MS.DAY + 1), 
 
+  /** 
+ * Переводит дату на value вперед у экземпляра класса 
+ * immutable
+ */ 
+  changeDays: (date: Date, value: number) => 
+  { 
+    date = copyDate(date);
+    date.setDate(date.getDate() + value); 
+  
+    return date;
+  }
+};
 
 export const sum_of_day_of_month = integral(
   [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 );
 
 
-/** 
- * Переводит дату на value вперед у экземпляра класса 
- * immutable
- */ 
-export const changeDays = (date: Date, value: number) => 
-{ 
-  date = copyDate(date);
-  date.setDate(date.getDate() + value); 
-  
-  return date;
-};
-
 /**
  * Copy date instance
  */
 export const copyDate = (date: Date) => new Date(date.getTime());
 
+// TODO а нужно ли сводить всё к дням? Не проще ли взять какой это день по порядку от юникс тайма?
 /**
  * Это один день?
  */
+
 export const equailtyDay = (d1: Date, d2: Date) => 
   d1.getFullYear() === d2.getFullYear() &&
   d1.getMonth() === d2.getMonth() &&
@@ -206,6 +214,7 @@ export const greaterThan = (d1: Date, d2: Date) =>
 const lessThan = (d1: Date, d2: Date) => !greaterThan(d1, d2);
 const lessOrEquality = (d1: Date, d2: Date) => lessThan(d1, d2) || equailtyDay(d1, d2);
 const greaterOrEquality = (d1: Date, d2: Date) => greaterThan(d1, d2) || equailtyDay(d1, d2);
+
 
 export const daysCompare = (d1: Date, operator: '==' | '>' | '<' | '>=' | '<=', d2: Date) =>
 {

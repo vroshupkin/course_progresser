1e36943ba8743592c5c589fd17c5f52118a43a70
Vadim <vroshupkin1@gmail.com>
2023-08-12 04:14:48 +0300

    Добавлен выбор даты в график

--- src/pages/IncomeBars/IncomeBars.page.tsx
+++ src/pages/IncomeBars/IncomeBars.page.tsx
@@ -10,8 +10,12 @@ import { userStore } from '../../components/stores/user.store';
 import { Navigate, redirect } from 'react-router-dom';
 import { PrivatePage } from '../Private.page';
 import { makeAutoObservable } from 'mobx';
-
-
+import { Properties as CSSProperties } from 'csstype';
+import { TCaloriesData } from './Calories.data';
+import { DateHelper, MONTH_NAME, WEEK, copyDate, equailtyDay } from '../../common/date.helper';
+import { caloriesData } from './Calories.data';
+import { applyStyle } from '../../common/css.helper';
+import { range } from '../../common/generator';
 const income_bar_store = new IncomeBarStore(100, 80);
 
 const income_bar_stores: IncomeBarStores = new IncomeBarStores(
@@ -46,25 +50,26 @@ const table_store = new TableStore([
 
 export const IncomeBarsPage: FC<{a: string}> = observer(() => 
 {
-  const classes = createUseStyles({
-    main_font: {
-      '& *': {
-        fontFamily: 'Anonymous Pro',
-        fontSize: '14px',
-        boxSizing: 'content-box'
-      },
-    },
-  })();
+  // const classes = createUseStyles({
+  //   main_font: {
+  //     '& *': {
+  //       fontFamily: 'Anonymous Pro',
+  //       fontSize: '14px',
+  //       boxSizing: 'content-box'
+  //     },
+  //   },
+  // })();
 
 
   return(
     <PrivatePage>
-      <div className={classes.main_font}>
+      <div className='helveticaNeueCyr text-[14px]'>
         <div>
           <IncomeBars stores={income_bar_stores}></IncomeBars>
-          <DaySelector/>
 
+          <MainChart/>
           <IncomeBarControl stores={income_bar_stores}></IncomeBarControl>
+
           <Table store={table_store}></Table>
         </div>
       </div>
@@ -218,33 +223,79 @@ const Arrow = (props: arrowProps) =>
 };
 
 
-export const DaySelector: FC<{}> = observer(() => 
+const MainChart = () => 
+{
+  const [ startDay, setStartDay ] = useState(new Date());
+  const [ selectDay, setSelectDay ] = useState(new Date());
+
+  
+  return(
+    <div style={{ display: 'flex', flexDirection: 'column' }} className='helveticaNeueCyr'>
+      <CaloriesChart calories={caloriesData} maxHeight={222}  maxValue={3000} numberOfDays={14} startDay={startDay}/>
+      <DaySelector startDay={startDay} setStartDay={setStartDay} selectDay={selectDay} setSelectDay={setSelectDay} numberDays={14}/>
+      <ShowSelectDay selectDay={selectDay}/>
+    </div>
+  );
+};
+
+const ShowSelectDay = (props: {selectDay: Date}) => 
+{
+  const getDateString = (date: Date) => 
+  {
+    let month =  MONTH_NAME.ru.genetive[date.getMonth()];
+    month = month[0].toUpperCase() + month.slice(1);
+    
+    return(
+      `${date.getDate()} ${month} ${date.getFullYear()}`    
+    );
+
+  };
+    
+  return(
+    <div className='flex flex-row text-[18px]'>
+      <div className='font-[550] text-[#505359] mr-[140px]'>Сегодня</div>
+      <div className='mr-[139px] text-[#838890]'>{getDateString(props.selectDay)}</div>
+      <div className=' text-[18px] font-[550] text-[#505359] flex flex-row gap-[15px] cursor-pointer'>
+        <div>Н</div>
+        <div className='text-[#21BEEF]'>2Н</div>
+        <div>М</div>
+        <div>6М</div>
+        <div>Г</div>
+      </div>
+    </div>
+  );
+      
+};
+
+type DaySelectorProps = {
+  startDay: Date,
+  selectDay: Date,
+  numberDays: number,
+  setStartDay: (date: Date) => void,
+  setSelectDay: (date: Date) => void,
+}
+
+/**
+ * Ячейка с числом даты
+ */
+export const DaySelector: FC<DaySelectorProps> = observer((props) => 
 {
   const classList = graphWeekSelector_class();
   const ref_div = useRef<HTMLDivElement>(null);
-  const [ order, setOrder ] = useState(-1);
-  
 
-  const handler: React.MouseEventHandler = (e) => 
-  {
-    if(e.currentTarget)
-    {
-      setOrder(getOrderInSiblings(e.currentTarget as HTMLElement));
-    }
-  };
-  
-  const [ selectDate, setSelectDate ] = useState(new Date());
-  const [ dayStart, setDayStart ] = useState(new Date());
+  const [ selectDate, setSelectDate ] = [ props.selectDay, props.setSelectDay ];
+  const [ startDay, setStartDay ] = [ props.startDay, props.setStartDay ];
+
 
   const changeStartDate = (count: number) => () =>
-    setDayStart(changeDays(dayStart, count));
+    setStartDay(DateHelper.changeDays(startDay, count));
 
   return(
     <div className={classList.container} ref={ref_div}>
 
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-end' }}>
         <Arrow type='<' onClick={changeStartDate(-1)}/>
-        <OneWeek setActiveDate={setSelectDate} day_counts={14} day_start={dayStart} selectDate={selectDate}/>
+        <OneWeek setActiveDate={setSelectDate} day_counts={props.numberDays} day_start={startDay} selectDate={selectDate}/>
         <Arrow type='>' onClick={changeStartDate(1)}/>
       </div>
     </div>
@@ -261,18 +312,70 @@ type OneWeekProps =
   setActiveDate?: Function
 }
 
+
+type CaloriesBarProps = 
+{
+  date: Date,
+  val: number,
+  maxVal: number,
+  maxHeightPx: number
+} 
+
+
+const CaloriesBar = (props: CaloriesBarProps) => 
+{
+  
+  const divRef = useRef<HTMLDivElement | null>(null);
+  // const divContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => 
+  {
+    if(divRef.current)
+    {
+      const style: CSSProperties = {
+        // width: '29px',
+        backgroundColor: '#21BEEF',
+        borderRadius: '5px 5px 0px 0px',
+        marginBottom: '10px'
+      };
+
+      applyStyle(divRef.current, style);
+
+    }
+  }, []);
+
+  useEffect(() => 
+  {
+    if(divRef.current)
+    {
+      const height = props.maxHeightPx * props.val / props.maxVal;
+      divRef.current.style.height = height + 'px';
+    }
+
+
+  }, [ props.maxHeightPx, props.maxVal, props.val ]);
+
+  
+  return(
+    <div style={{ width: '39px', display: 'flex', justifyContent: 'center' }}>
+      <div style={{ width: '10px' }} ref={divRef}></div>
+    </div>
+  );
+};
+
 const OneWeek = (props: OneWeekProps) => 
 {
   const classList = graphWeekSelector_class();
 
-  const days = range(props.day_counts).map(v => changeDays(props.day_start, v));
+  const days = 
+    range(props.day_counts)
+      .map(v => DateHelper.changeDays(props.day_start, v));
 
   const changeSelectedDate = (e: React.MouseEvent) => 
   {
     if(e.currentTarget && props.setActiveDate)
     {
       const order = getOrderInSiblings(e.currentTarget as HTMLElement);
-      
       props.setActiveDate(days[order]);
     }
       
@@ -283,12 +386,13 @@ const OneWeek = (props: OneWeekProps) =>
       {
         days
           .map((d, i) => 
-            <DayCell 
-              type={d.getDay() == 1 % 7? 'left' : d.getDay() % 7 == 0 ? 'right' : 'common'}
-              onClick={changeSelectedDate}
-              children={d.getDate()}
-              select={ equailtyDay(d, props.selectDate) }
-            />
+            <div style={{ display: 'flex', flexDirection: 'column' }} onClick={changeSelectedDate}>
+              <DayCell 
+                type={d.getDay() === WEEK.Monday? 'left' : d.getDay() === WEEK.Sunday ? 'right' : 'common'}
+                children={d.getDate()}
+                select={ equailtyDay(d, props.selectDate) }
+              />
+            </div>
           )
       }
 
@@ -296,51 +400,144 @@ const OneWeek = (props: OneWeekProps) =>
   );
 };
 
-/**
- * @example range(6) => [0, 1, 2, 3, 4, 5]; range(2, 6) => [2, 3, 4, 5]
- */
-const range = (start: number, end?: number) => 
+
+type CaloriesChartProps = {
+  calories: TCaloriesData[],
+  maxHeight: number,
+  maxValue: number,
+  startDay: Date,
+  numberOfDays: number
+}
+
+
+const CaloriesChart = (props: CaloriesChartProps) => 
 {
-  if(end === undefined)
+  const containerDivRef = useRef<null | HTMLDivElement>(null);
+  
+  useEffect(() => 
   {
-    end = start;
-    start = 0;
-  }
+    const style: CSSProperties = {
+      height: '222px',
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'end'
+    };
+
+    if(containerDivRef.current)
+    {
+      applyStyle(containerDivRef.current, style);
+    }
+    
+  }, []);
+
+  const [ startDay, numberOfDays ] = [ props.startDay, props.numberOfDays ];
+
   
-  return new Array(end- start).fill(0).map((_, i) => i + start);  
+  const createCaloriesMap = () => 
+  {
+    const daysFilterFn = (date: Date): Boolean => 
+    {
+      const day_of_date = DateHelper.DateToDayOrder(date);
+      const day_of_start_day = DateHelper.DateToDayOrder(startDay);
+      const day_of_last_day = DateHelper.DateToDayOrder(DateHelper.changeDays(startDay, numberOfDays - 1));
+
+      return(
+        day_of_date >= day_of_start_day && 
+        day_of_date <= day_of_last_day
+      );
+    };
+
+    const calories_dict = {} as Record<number, number>;
+    
+    range(numberOfDays)
+      .map(val => DateHelper.changeDays(startDay, val))
+      .forEach(date => calories_dict[DateHelper.DateToDayOrder(date)] = 0);
+
+    props.calories
+      .filter(data => daysFilterFn(data.date))
+      .forEach(data => 
+      {
+        const dayOrder = DateHelper.DateToDayOrder(data.date);
+        if(calories_dict[dayOrder] == 0)
+        {
+          calories_dict[dayOrder] = data.val;
+        }
+      });
+
+
+    return calories_dict;
+        
+  };
+  
+  return(
+    <div ref={containerDivRef}>
+      <div style={{ position: 'absolute' }}>
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        
+          <line x1="0" y1="80" x2="100" y2="20" stroke="black" />
+
+        </svg>
+      </div>
+      
+      <div style={{ width: '39px' }}/>
+      {
+        Object.entries(createCaloriesMap()).
+          map(([ dayOrder, val ]) => 
+          {
+            const date = DateHelper.DayOrderToDate(Number(dayOrder));
+              
+            return(
+              <>
+                <CaloriesBar date={date} val={val} maxVal={props.maxValue} maxHeightPx={props.maxHeight}/>
+                {date.getDay() === WEEK.Sunday && <div style={{ marginRight: '37px' }}></div>}
+              </>
+            );
+          }
+          )
+
+      }
+     
+      
+    </div>
+    
+  );
 };
 
+type ChartTittlesProps = {
+  maxValue: number,
+  step: number,
+  maxHeight: number
+}
 
-/** 
- * Переводит дату на value вперед у экземпляра класса 
- * immutablecp[yq]
- */ 
-const changeDays = (date: Date, value: number) => 
-{ 
-  date = copyDate(date);
-  date.setDate(date.getDate() + value); 
-  
-  return date;
+const ChartTittles = (props: ChartTittlesProps) => 
+{
+
+  useEffect(() => 
+  {
+    if(divContainerRef.current)
+    {
+      const style: CSSProperties = {
+        display: 'flex'
+
+      };
+
+      applyStyle(divContainerRef.current, style);
+    }
+    
+  }, []);
+
+  const divContainerRef = useRef<HTMLDivElement>(null);
+
+  return(
+    <div ref={divContainerRef}>
+
+
+    </div>
+  );
 };
 
-/**
- * Copy date instance
- */
-const copyDate = (date: Date) => new Date(date.getTime());
-
-/**
- * Это один день?
- */
-const equailtyDay = (d_1: Date, d_2: Date) =>   
-  d_1.getFullYear() === d_2.getFullYear() &&
-  d_1.getMonth() === d_2.getMonth() &&
-  d_1.getDate() === d_2.getDate();
-
-
-const date_now = new Date();
-const week = range(7).map(v => changeDays(copyDate(date_now), v));
-
-// console.log(week.map(d => d.getDay()));
+// const date_now = new Date();
+// const week = range(7).map(v => DateHelper.changeDays(copyDate(date_now), v));
 
 
 /**
@@ -364,4 +561,5 @@ const getOrderInSiblings  = (elem: HTMLElement) =>
 
   return -1;
 };
+
 

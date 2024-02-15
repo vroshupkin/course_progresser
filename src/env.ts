ceb3983e519120c76c18f54860e1e6b5b06768a4
Vadim <vroshupkin1@gmail.com>
2024-02-15 21:42:25 +0300

    env убран из отслеживания

--- src/env.ts
+++
@@ -1,9 +0,0 @@
-
-import { ENV as ENV_EXAMPLE } from './env.example';
-
-
-export const ENV: typeof ENV_EXAMPLE = {
-  BACKEND_URL: 'http://vroshupkin1.ddns.net:3000/',
-  DDNS_HOST:  'http://vroshupkin1.ddns.net/'
-  
-};

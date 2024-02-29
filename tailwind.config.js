62f04f8ff0bc7085e5bcb1040a717d73cb98b463
Vadim <vroshupkin1@gmail.com>
2024-02-29 07:26:34 +0300

    Добавлен tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
@@ -13,5 +14,8 @@ module.exports = {
     }
   },
   plugins: [],
+  safelist: [
+    'bg-green'
+  ]
 };
 
